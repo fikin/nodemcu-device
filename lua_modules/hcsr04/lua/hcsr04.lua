66a766f463a7ee28df91d3d4b11ae1ee154c0afa
Nikolay Fiykov <nikolai.fiikov@gmail.com>
2024-03-13 17:13:13 +0200

    skeleton ultrasonic sensor integration, more work is needed.

--- lua_modules/hcsr04/lua/hcsr04.lua
+++ lua_modules/hcsr04/lua/hcsr04.lua
@@ -1,6 +1,6 @@
 local modname = ...
 
----@alias hcsr04_sensor_reading_cb fun(ok:boolean,distanceMm:number|nil,err:any|nil)
+---@alias hcsr04_sensor_reading_cb fun(distanceMm:number)
 
 ---@class hcsr04_sensor_cfg
 ---@field triggerPin integer
@@ -12,14 +12,15 @@ local modname = ...
 local node, tmr, gpio = require("node"), require("tmr"), require("gpio")
 
 -- speed of sound in air at 20C is about 343m/s
--- metrics conversion to mm/ss = (100cm * 10mm) / (1000ms * 1000us)
+-- metrics conversion to mm/us = (100cm * 10mm) / (1000ms * 1000us)
 -- converted to one CPU tick (ccount) based on mm/us
--- divided by 2 because the meassured duration includes both directions of sound travel
+-- divided by 2 because the meassured time duration includes both directions of sound travel
 local tickMmUs = 0.343 * node.getcpufreq() / 2
 
 --- configuration as provided by caller
 ---@type hcsr04_sensor_cfg
-local cfg
+local cfg = require("device-settings")(modname)
+
 --- callback to pass the read distance as provided by caller
 ---@type hcsr04_sensor_reading_cb
 local onReadingCb
@@ -44,51 +45,52 @@ local function meassureDistance()
     node.delay(cfg.impulseUs)
     gpio.write(cfg.triggerPin, gpio.LOW)
 
-    local ccountStart = coroutine.yield(co)
-    local ccountEnd = coroutine.yield(co)
+    local ccountStart = coroutine.yield()
+    local ccountEnd = coroutine.yield()
 
     tm:stop()
 
     return math.abs(ccountEnd - ccountStart) * cfg.calibrationCoef / tickMmUs
 end
 
+---average distance reads
+---@return number
 local function meassureAverage()
     local sum = 0
-    for i = 0, cfg.averageReads do
+    for _ = 0, cfg.averageReads do
         sum = sum + meassureDistance()
     end
 
-    -- tear down sequence
-    tm:unregister()
-    gpio.trig(cfg.echoPin, "none", nil)
-
-    onReadingCb(sum / cfg.averageReads)
+    return sum / cfg.averageReads
 end
 
----@param cfg1 hcsr04_sensor_cfg
----@param onReadingCb1 hcsr04_sensor_reading_cb
-local function performMeassurement(cfg1, onReadingCb1)
-    tm = tmr.create()
-    cfg = cfg1
-    onReadingCb = onReadingCb1
-
+local function performMeassurement()
     gpio.mode(cfg.triggerPin, gpio.OUTPUT)
     gpio.mode(cfg.echoPin, gpio.INPUT)
 
     gpio.trig(cfg.echoPin, "both", yieldCcount)
 
     -- indefinite distance protection, set to 50ms i.e. ~ 17m
+    tm = tmr.create()
     tm:register(50, tmr.ALARM_AUTO, yieldCcount)
 
-    co = coroutine.create(meassureAverage)
+    local distance = meassureAverage()
+
+    -- tear down sequence
+    tm:unregister()
+    gpio.trig(cfg.echoPin, "none", nil)
+
+    onReadingCb(distance)
 end
 
----@param cfg1 hcsr04_sensor_cfg
 ---@param onReadingCb1 hcsr04_sensor_reading_cb
-local function main(cfg1, onReadingCb1)
+local function main(onReadingCb1)
     package.loaded[modname] = nil
 
-    performMeassurement(cfg1, onReadingCb1)
+    onReadingCb = onReadingCb1
+
+    co = coroutine.create(performMeassurement)
+    coroutine.resume(co)
 end
 
 return main
