622e9013fc2176c70e216f344637e2277385650f
Nikolay Fiykov <nikolai.fiikov@gmail.com>
2024-05-31 15:58:46 +0300

    linter errors

--- integration-tests/lua/testBrokenLFSImg.lua
+++ integration-tests/lua/testBrokenLFSImg.lua
@@ -2,14 +2,13 @@ local lu = require("luaunit")
 local nodemcu = require("nodemcu")
 
 local file = require("file")
-local sjson = require("sjson")
 
 ---test that LFS.img is LFS.reload() and error is captured.
 ---as node.LFS.reload() is not possible to implement currently.
 
 function testInit()
     nodemcu.reset()
-    
+
     --lu.assertIsTrue(file.exists("LFS.img"))
     lu.assertIsFalse(file.exists("LFS.img.PANIC.txt"))
     lu.assertIsTrue(file.exists("LFS.img"))
