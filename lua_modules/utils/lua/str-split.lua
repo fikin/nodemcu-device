09755adcfb3074a25cf6981e1149d967add7977c
Nikolay Fiykov <nikolai.fiikov@gmail.com>
2024-08-17 17:35:52 +0300

    mainstreaming loop logic

--- lua_modules/utils/lua/str-split.lua
+++ lua_modules/utils/lua/str-split.lua
@@ -7,16 +7,18 @@ local modname = ...
 local function main(text, delimiter)
   package.loaded[modname] = nil
 
-  local result               = {}
-  local from                 = 1
-  local delim_from, delim_to = string.find(text, delimiter, from)
-  while delim_from do
+  local result            = {}
+  local from              = 1
+  while true do
+    local delim_from, delim_to = string.find(text, delimiter, from)
+    if not delim_from then break end
     table.insert(result, string.sub(text, from, delim_from - 1))
-    from                 = delim_to + 1
-    delim_from, delim_to = string.find(text, delimiter, from)
+    from = delim_to + 1
   end
   table.insert(result, string.sub(text, from))
   return result
 end
+
+
 
 return main
